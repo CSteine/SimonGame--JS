13d2b2079c26890e3d73ca5f46826877b4d4aac9
Csteine <candace@candymuse.me>
2018-11-27 07:48:49 -0500

    Added button clciked action

--- games.js
+++ games.js
@@ -1,5 +1,14 @@
 const buttonColors = ['red', 'blue', 'green', 'yellow'];
 let gamePattern = [];
+let userClickedPattern = [];
+
+
+$("btn").click(function() {
+    let userChosenColor = $(this).attr('id');
+    userClickedPattern.push(userChosenColor)
+})
+
+
 
 
 
@@ -8,5 +17,11 @@ function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4) + 1;
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    $ { "#" + randomChosenColor }.fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
+
+function playSound(name) {
+
 }
